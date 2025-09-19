53315bf86d44c4b086460462590ea62592bce8e7
the.hvr <thehvr1@gmail.com>
2025-09-19 14:30:15 +0530

    add app.js file

---
+++ app.js
@@ -0,0 +1,12 @@
+//Branch  feature01
+
+
+//this is the sub=branch of main branch.
+
+//git branch  => to check branch name
+//git branch -m main => to rename branch
+//git checkout branchName => to navigate 
+//git checkout -b branchName  => to create new branch
+//git branch -d => to delete existing branch 
+
+//git push --set-upstream origin feature01  => to set upstream for feature branch
